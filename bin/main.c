86b7ee9cfa921777a91e45b5e632f90b4eeac271
kumudiaorong <kmdr.error@gmail.com>
2024-04-20 12:32:33 +0800

    fix:easy example

--- bin/main.c
+++ bin/main.c
@@ -1,12 +1,12 @@
 void main()
 {
-         int A[100];
+         int A[5];
          int i;
-         for(i=0;i<100;i++)
+         for(i=0;i<5;i++)
              A[i]=i;
-         for(i=1;i<100;i++)
+         for(i=1;i<5;i++)
              A[i]=A[i-1]+1000;
-         if (i>5){
+         if (i>3){
              A[50]=10;
          }
 }
